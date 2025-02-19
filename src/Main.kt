80091acb760051631a100f3bad8eb2bfbab7a42f
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-19 11:49:46 +0300

    Commit Message

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,28 @@
+import UserRole
+
+fun checkAccessLevel(user: UserRole){
+    if (user.accessLevel==0){
+        println("You are an admin. You have the highest access level.")
+    }
+
+    if (user.accessLevel==1){
+        println("You are an editor. You have access to edit stuff (idk).")
+    }
+
+    if (user.accessLevel==2){
+        println("you are a viewer. You have basic access level.")
+    }
+}
+
 fun main() {
-    println("Hello World!")
+
+    var admin: UserRole = UserRole.ADMIN
+    var editor: UserRole = UserRole.EDITOR
+    var viewer: UserRole = UserRole.VIEWER
+
+    checkAccessLevel(admin)
+
+    for (user in UserRole.entries){
+        println(user)
+    }
 }
---
+++ src/UserRole.kt
@@ -0,0 +1,6 @@
+//part 1 and 2
+enum class UserRole (val accessLevel:Int){
+    ADMIN(0),
+    EDITOR(1),
+    VIEWER(2),
+}
